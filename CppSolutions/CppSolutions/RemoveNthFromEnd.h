22f78b37742d0ab236a303a4664817b4db46c74c
MKSherbini <mksherbini@gmail.com>
2021-12-11 00:06:36 +0200

    RemoveNthFromEnd

---
+++ CppSolutions/CppSolutions/RemoveNthFromEnd.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "stdc++.h"
+using namespace std;
+
+//https://leetcode.com/problems/middle-of-the-linked-list/
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class RemoveNthFromEnd
+{
+public:
+	// 0 ms, faster than 100.00% : 10.6 MB, less than 73.81%
+	ListNode* solution(ListNode* head, int n) {
+		auto it = head, slowerIt = it;
+		for (int i = 0; i < n; i++)
+			it = it->next;
+		if (!it) return head->next;
+		slowerIt = head;
+		while (it->next)
+			it = it->next, slowerIt = slowerIt->next;
+		slowerIt->next = slowerIt->next->next;
+		return head;
+	}
+};
+
