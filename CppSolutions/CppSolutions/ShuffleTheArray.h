8c895f0c4b117af8c71391694a2731e4de6ae0b2
MKSherbini <mksherbini@gmail.com>
2021-12-26 18:50:54 +0200

    ShuffleTheArray

--- CppSolutions/CppSolutions/ShuffleTheArray.h
+++ CppSolutions/CppSolutions/ShuffleTheArray.h
@@ -17,3 +17,10 @@ public:
 };
 // x => i*2
 // y => (i-n)*2+1 
+
+//run(ShuffleTheArray(),
+//	vector<int> { 2, 3, 5, 4, 1, 7 },
+//	vector<int> { 2, 5, 1, 3, 4, 7 }, 3);
+//run(ShuffleTheArray(),
+//	vector<int> { 1, 4, 2, 3, 3, 2, 4, 1 },
+//	vector<int> { 1, 2, 3, 4, 4, 3, 2, 1 }, 4);
