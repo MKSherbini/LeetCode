d6e2b71684c87e02bda4c878a4fc58de8ac674ca
MKSherbini <mksherbini@gmail.com>
2022-01-10 15:44:35 +0200

    optimize helpers

--- CppSolutions/CppSolutions/Helpers.h
+++ CppSolutions/CppSolutions/Helpers.h
@@ -4,22 +4,15 @@ using namespace std;
 
 template<typename T>
 static void print(const T& v) {
-	cout << v << endl;
+	cout << v << " ";
 }
 
 template<typename T>
 static void print(const vector<T>& v) {
 	for (auto a : v) {
-		cout << a << " ";
-	}
-	cout << endl;
-}
-
-template<typename T>
-static void print(const vector< vector<T> >& v) {
-	for (auto a : v) {
 		print(a);
 	}
+	cout << endl;
 }
 
 template<typename T>
@@ -37,21 +30,11 @@ static int8_t valid(const vector<T>& v, const vector<T>& v2) {
 	return 1;
 }
 
-template<typename T>
-static int8_t valid(const vector< vector<T> >& v, const vector< vector<T> >& v2) {
-	if (v.size() != v2.size()) return 0;
-
-	for (int i = 0; i < v.size(); i++)
-		if (!valid(v[i], v2[i])) return 0;
-
-	return 1;
-}
-
 template<typename T, typename R, typename ...Args>
 void run(T t, R res, Args ... r) {
 	R ret = t.solution(r...);
 	print(ret);
-	cout << "judge: " << (valid(ret, res) ? "Accepted" : "Wrong answer") << endl;
+	cout << "judge: " << (valid(ret, res) ? "Accepted" : "Wrong answer") << endl << endl;
 }
 
 template<typename T, int params, typename std::enable_if<params == 1, int*>::type = nullptr>
