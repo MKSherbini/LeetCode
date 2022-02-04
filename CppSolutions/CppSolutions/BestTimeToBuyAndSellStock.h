bb5d55b99d23e2316639fc9b8fdcf3bf05e162fa
MKSherbini <mksherbini@gmail.com>
2022-02-04 23:22:41 +0200

    BestTimeToBuyAndSellStock

---
+++ CppSolutions/CppSolutions/BestTimeToBuyAndSellStock.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "stdc++.h"
+using namespace std;
+
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class BestTimeToBuyAndSellStock
+{
+public:
+	// 112 ms, faster than 89.47% : 97.1 MB, less than 6.92%
+	int solution(vector<int>& prices) {
+		if (prices.size() == 1) return 0;
+
+		vector<int> maxes(prices);
+		int profit = 0;
+		for (int i = prices.size() - 2; i >= 0; i--)
+			maxes[i] = max(maxes[i], maxes[i + 1]);
+
+		for (int i = 0; i < prices.size(); i++)
+			profit = max(maxes[i] - prices[i], profit);
+
+		return profit;
+	}
+	vector<int> output = {
+		5,
+		0,
+	};
+	static constexpr int const& inputs = 1;
+	vector<vector<int>> input1 = {
+		{7,1,5,3,6,4},
+		{7,6,4,3,1}
+	};
+};
+
--- CppSolutions/CppSolutions/Runner.cpp
+++ CppSolutions/CppSolutions/Runner.cpp
@@ -1,4 +1,4 @@
-#include "TruncateSentence.h"
+#include "BestTimeToBuyAndSellStock.h"
 #include "stdc++.h"
 
 // todo improve printing + examples running
@@ -6,5 +6,5 @@
 int main()
 {
 	freopen("output.txt", "w", stdout);
-	Runner(true).run<TruncateSentence>();
+	Runner(true).run<BestTimeToBuyAndSellStock>();
 }
