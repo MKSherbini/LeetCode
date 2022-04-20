a5a1f9e8e8065c7b594a38e8277f0bf84f4dcaa1
MKSherbini <mksherbini@gmail.com>
2022-04-20 16:49:58 +0200

    WordBreak

--- CppSolutions/CppSolutions/Runner.cpp
+++ CppSolutions/CppSolutions/Runner.cpp
@@ -1,4 +1,4 @@
-﻿#include "DecodeWays.h"
+﻿#include "WordBreak.h"
 #include "stdc++.h"
 
 // todo improve printing + examples running
@@ -11,5 +11,5 @@ int main()
 {
 	freopen("output.txt", "w", stdout);
 	freopen("input.txt", "r", stdin);
-	Runner(true).run<DecodeWays>();
+	Runner(true).run<WordBreak>();
 }
---
+++ CppSolutions/CppSolutions/WordBreak.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "stdc++.h"
+using namespace std;
+
+//https://leetcode.com/problems/word-break/
+
+class WordBreak
+{
+public:
+	// 0 ms, faster than 100% : 7.5 MB, less than 95.42%
+	bool solution(string s, vector<string> wordDict) {
+		vector<int> mem(s.size(), -1);
+		auto ret = curse(s, wordDict, mem, 0);
+		Printer::print(mem);
+		return max(0, ret);
+	}
+	int curse(string& s, vector<string>& wordDict, vector<int>& mem, int i) {
+		if (i == s.size()) return 1;
+		if (mem[i] != -1) return mem[i];
+
+		int ret = 0;
+		for (auto word : wordDict) {
+			int sz = word.size();
+			if (s.substr(i, sz) == word) {
+				ret |= curse(s, wordDict, mem, i + sz);
+				if (ret) break;
+			}
+		}
+
+		return mem[i] = ret;
+	}
+	vector<int> output = {
+		true,
+		true,
+		false
+	};
+	static constexpr int const& inputs = 2;
+	vector<string> input1 = {
+		"leetcode",
+		"applepenapple",
+		"catsandog"
+	};
+	vector<vector<string>> input2 = {
+		{"leet","code"},
+		{"apple","pen"},
+		{"cats","dog","sand","and","cat"}
+	};
+};
