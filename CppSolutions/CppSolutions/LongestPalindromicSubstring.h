2ed4f4973412e103d69e7f00cfd7eebe162ba683
MKSherbini <mksherbini@gmail.com>
2022-05-16 21:52:39 +0200

    LongestPalindromicSubstring

--- CppSolutions/CppSolutions/LongestPalindromicSubstring.h
+++ CppSolutions/CppSolutions/LongestPalindromicSubstring.h
@@ -7,24 +7,46 @@ using namespace std;
 class LongestPalindromicSubstring
 {
 public:
-	// 2341 ms, faster than 5.01% : 387.3 MB, less than 5.03%
+	// 21 ms, faster than 87.69% : 6.9 MB, less than 99.83%
 	string solution(string s) {
+		int st = 0, limit = 1;
+		for (size_t i = 0; i < s.size(); i++)
+		{
+			int lo = i - 1;
+			int hi = i + 1;
+
+			while (lo >= 0 && s[i] == s[lo])
+				lo--;
+			while (hi < s.size() && s[i] == s[hi])
+				hi++;
+
+			while (lo >= 0 && hi < s.size() && s[lo] == s[hi])
+				hi++, lo--;
+
+			if (limit < hi - lo - 1)
+				st = lo + 1, limit = hi - lo - 1;
+		}
+		return s.substr(st, limit);
+	}
+
+	// 2341 ms, faster than 5.01% : 387.3 MB, less than 5.03%
+	string solution2(string s) {
 		vector<vector<int>> mem(s.size(), vector<int>(s.size(), -1));
 		pair<int, int> ans;
 		int mx = 0;
-		curse(s, mem, mx, ans, 0, s.size() - 1);
+		curse2(s, mem, mx, ans, 0, s.size() - 1);
 		Printer::print(mem);
 		return s.substr(ans.first, ans.second - ans.first + 1);
 	}
-	int curse(string& s, vector<vector<int>>& mem, int& mx, pair<int, int>& ans, int i, int j) {
+	int curse2(string& s, vector<vector<int>>& mem, int& mx, pair<int, int>& ans, int i, int j) {
 		if (i > j) return 0;
 		if (i == j) return mem[i][j] = 1;
 		if (mem[i][j] != -1) return mem[i][j];
-		curse(s, mem, mx, ans, i, j - 1);
-		curse(s, mem, mx, ans, i + 1, j);
+		curse2(s, mem, mx, ans, i, j - 1);
+		curse2(s, mem, mx, ans, i + 1, j);
 		int ret = 0;
 		if (s[i] == s[j])
-			ret = 2 + curse(s, mem, mx, ans, i + 1, j - 1);
+			ret = 2 + curse2(s, mem, mx, ans, i + 1, j - 1);
 		else ret = -OO;
 		if (ret > mx)
 			mx = ret, ans = { i,j };
