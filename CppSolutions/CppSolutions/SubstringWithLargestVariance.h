640b2fdcda65bd845173f48feb28f377bc0f294a
MKSherbini <mksherbini@gmail.com>
2022-05-22 03:15:29 +0200

    SubstringWithLargestVariance

--- CppSolutions/CppSolutions/SubstringWithLargestVariance.h
+++ CppSolutions/CppSolutions/SubstringWithLargestVariance.h
@@ -7,31 +7,31 @@ using namespace std;
 class SubstringWithLargestVariance
 {
 public:
-	//
+	// 138 ms, faster than 92.28% : 6.8 MB, less than 96.05%
 	int solution(string s) {
 		uint8_t vis[26] = { 0 };
 		for (size_t i = 0; i < s.size(); i++) vis[s[i] - 'a'] = 1;
 		int ans = 0;
 		for (char x = 'a'; x <= 'z'; x++)
 		{
+			if (!vis[x - 'a']) continue;
 			for (char y = 'a'; y <= 'z'; y++)
 			{
-				if (x == y || !vis[x - 'a'] || !vis[y - 'a']) continue;
-				int pre = 0, t = 0, t2 = 0, m = 99999;
+				if (x == y || !vis[y - 'a']) continue;
+				int pre = 0, t = 0;
 				for (size_t i = 0; i < s.size(); i++)
 				{
 					if (s[i] == x) {
 						++pre;
-						t = 1;
 						ans = max(ans, pre - 1);
 					}
 					else if (s[i] == y) {
 						--pre;
-						if (t == 1) t2 = 1;
+						t = 1;
 					}
-					if (t2)
+					if (t)
 						ans = max(ans, pre);
-					if (pre < 0) pre = 0, t = 0, t2 = 0;
+					if (pre < 0) pre = 0, t = 0;
 				}
 			}
 		}
