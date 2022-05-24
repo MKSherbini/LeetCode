418e42414eb46c7fc55f5fbe2b406b0c91a78a58
MKSherbini <mksherbini@gmail.com>
2022-05-24 21:52:08 +0200

    IntersectionOfTwoArraysIi

--- CppSolutions/CppSolutions/IntersectionOfTwoArraysIi.h
+++ CppSolutions/CppSolutions/IntersectionOfTwoArraysIi.h
@@ -7,7 +7,7 @@ using namespace std;
 class IntersectionOfTwoArraysIi
 {
 public:
-	// 0 ms, faster than 100% : 9.9 MB, less than 99.48%
+	// 0 ms, faster than 100% : 9.9 MB, less than 93.75%
 	vector<int> solution(vector<int> nums1, vector<int> nums2) {
 		sort(begin(nums1), end(nums1));
 		sort(begin(nums2), end(nums2));
@@ -16,9 +16,9 @@ public:
 		int i = 0, j = 0;
 
 		while (i < nums1.size() && j < nums2.size())
-		{
-
-		}
+			if (nums1[i] < nums2[j]) i++;
+			else if (nums1[i] > nums2[j]) j++;
+			else ans.push_back(nums1[i++]), j++;
 
 		return ans;
 	}
