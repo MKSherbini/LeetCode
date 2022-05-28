64e65d0962f4cb30677e38ec0cb6e1f6b0c0cfe8
MKSherbini <mksherbini@gmail.com>
2022-05-28 08:57:11 +0200

    MissingNumber

---
+++ CppSolutions/CppSolutions/MissingNumber.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "stdc++.h"
+using namespace std;
+
+//https://leetcode.com/problems/missing-number/
+
+class MissingNumber
+{
+public:
+	// 7 ms, faster than 99.9% : 17.9 MB, less than 93.84%
+	int solution(vector<int> nums) {
+		int total = (nums.size() * (nums.size() + 1)) / 2, sum = 0;
+		for (auto x : nums)
+			sum += x;
+
+		return total - sum;
+	}
+
+	// 15 ms, faster than 95.17% : 18.3 MB, less than 8.81%
+	int solution2(vector<int> nums) {
+		vector<int> vis(nums.size() + 1, 0);
+		for (auto x : nums) {
+			vis[x] = 1;
+		}
+		for (size_t i = 0; i < vis.size(); i++)
+		{
+			if (!vis[i]) return i;
+		}
+	}
+	vector<int> output = {
+		2,
+		2,
+		8
+	};
+	static constexpr int const& inputs = 1;
+	vector<vector<int>> input1 = {
+		{3,0,1},
+		{0,1},
+		{9,6,4,2,3,5,7,0,1}
+	};
+};
--- CppSolutions/CppSolutions/Runner.cpp
+++ CppSolutions/CppSolutions/Runner.cpp
@@ -1,4 +1,4 @@
-﻿#include "ValidSudoku.h"
+﻿#include "MissingNumber.h"
 #include "stdc++.h"
 
 // todo improve printing + examples running
@@ -11,5 +11,5 @@ int main()
 {
 	freopen("output.txt", "w", stdout);
 	freopen("input.txt", "r", stdin);
-	Runner(true).run<ValidSudoku>();
+	Runner(true).run<MissingNumber>();
 }
