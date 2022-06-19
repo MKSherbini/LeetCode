b4745013398db466124147c896a88dc57d09b7f1
MKSherbini <mksherbini@gmail.com>
2022-06-19 22:11:23 +0200

    CombinationSum

--- CppSolutions/CppSolutions/CombinationSum.h
+++ CppSolutions/CppSolutions/CombinationSum.h
@@ -7,13 +7,14 @@ using namespace std;
 class CombinationSum
 {
 public:
-	// 234 ms, faster than 10.66% : 79.3 MB, less than 10.97%
+	// 3 ms, faster than 97.66% : 17.1 MB, less than 39.14%
 	vector<vector<int>> solution(vector<int> candidates, int target) {
 		vector<vector<int>> ans;
-		curse(candidates, ans, target, 0, {});
+		vector<int> r;
+		curse(candidates, ans, target, 0, r);
 		return ans;
 	}
-	void curse(vector<int>& nums, vector<vector<int>>& ans, int sum, int i, vector<int> row) {
+	void curse(vector<int>& nums, vector<vector<int>>& ans, int sum, int i, vector<int>& row) {
 		if (sum == 0) return ans.push_back(row);
 		if (i == nums.size() || sum < 0) return;
 
@@ -21,6 +22,23 @@ public:
 		curse(nums, ans, sum, i + 1, row);
 		row.push_back(nums[i]);
 		curse(nums, ans, sum - nums[i], i, row);
+		row.pop_back();
+	}
+
+	// 226 ms, faster than 13.03% : 79.3 MB, less than 10.97%
+	vector<vector<int>> solution2(vector<int> candidates, int target) {
+		vector<vector<int>> ans;
+		curse2(candidates, ans, target, 0, {});
+		return ans;
+	}
+	void curse2(vector<int>& nums, vector<vector<int>>& ans, int sum, int i, vector<int> row) {
+		if (sum == 0) return ans.push_back(row);
+		if (i == nums.size() || sum < 0) return;
+
+
+		curse2(nums, ans, sum, i + 1, row);
+		row.push_back(nums[i]);
+		curse2(nums, ans, sum - nums[i], i, row);
 	}
 
 	vector<vector<vector<int>>> output = {
