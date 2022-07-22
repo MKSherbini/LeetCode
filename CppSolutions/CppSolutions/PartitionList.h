dfa9d82e2c9d7e4a364ab6b1b83e1bca12f5a6b7
MKSherbini <mksherbini@gmail.com>
2022-07-22 16:15:20 +0200

    PartitionList

---
+++ CppSolutions/CppSolutions/PartitionList.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "stdc++.h"
+using namespace std;
+
+//https://leetcode.com/problems/partition-list/
+
+class PartitionList
+{
+public:
+	// 0 ms, faster than 100% : 10.1 MB, less than 99.85%
+	ListNode* solution(ListNode* head, int x) {
+		if (!head) return head;
+
+		ListNode* cur = head, * prev = NULL;
+
+		while (cur && cur->val >= x)
+		{
+			prev = cur;
+			cur = cur->next;
+		}
+		if (!cur) return head;
+
+		if (prev) {
+			prev->next = cur->next;
+			cur->next = head;
+			head = cur;
+			cur = prev->next;
+		}
+
+		auto smallsIT = head;
+
+		while (cur)
+		{
+			if (cur->val < x && prev)
+			{
+				if (prev == smallsIT) {
+					smallsIT = cur;
+					prev = cur;
+					cur = cur->next;
+					continue;
+				}
+				prev->next = cur->next;
+				cur->next = smallsIT->next;
+				smallsIT->next = cur;
+				smallsIT = cur;
+				cur = prev->next;
+			}
+			else {
+				prev = cur;
+				cur = cur->next;
+			}
+		}
+
+		return head;
+	}
+	vector<vector<int>> output = {
+		{1,2,2,4,3,5},
+		{1,2}
+	};
+	static constexpr int const& inputs = 2;
+	vector<vector<int>> input1 = {
+		{1,4,3,2,5,2},
+		{2,1}
+	};
+	vector<int> input2 = {
+		3,
+		2
+	};
+};
