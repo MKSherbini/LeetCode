facfc0f67c4a7fb41d452374a1bccd7e8cced4ba
MKSherbini <mksherbini@gmail.com>
2022-08-20 17:25:28 +0200

    SplitArrayIntoConsecutiveSubsequences

--- CppSolutions/CppSolutions/SplitArrayIntoConsecutiveSubsequences.h
+++ CppSolutions/CppSolutions/SplitArrayIntoConsecutiveSubsequences.h
@@ -7,38 +7,39 @@ using namespace std;
 class SplitArrayIntoConsecutiveSubsequences
 {
 public:
-	//// 165 ms, faster than 50.77% : 69.5 MB, less than 12.99%
-	//bool solution(vector<int> nums) {
-	//	vector f(2003, 0);
-	//	for (auto x : nums)
-	//		f[x + 1000]++;
+	// 75 ms, faster than 99.53% : 56.5 MB, less than 78.51%
+	bool solution(vector<int> nums) {
+		vector f(2003, 0);
+		for (auto x : nums)
+			f[x + 1000]++;
 
-	//	priority_queue<int, vector<int>, greater<>> left;
-	//	priority_queue<int, vector<int>, greater<>> right;
-	//	right.push(f[1]);
+		Printer::print(f);
+		for (size_t i = 0; i < size(f); i++)
+		{
+			int t = f[i];
+			int l = 0, m = 0, r = 0;
+			if (i > 1)
+				l = min(f[i - 1], f[i - 2]);
+			if (i < size(f) - 1 && i > 0)
+				m = min(f[i - 1], f[i + 1]);
 
-	//	for (size_t i = 0; i < size(f); i++)
-	//	{
-	//		if (i < size(f) - 2) right.push(f[i + 2]);
-	//		if (i > 0) left.push(f[i]);
+			int lm = 0;
+			if (i > 0)
+				lm = min(l + m, f[i - 1]);
+			if (i < size(f) - 2)
+				r = min(f[i + 1] - lm + l, f[i + 2]);
 
-	//		vector t(begin(f) + max(0, (int)i - 2), begin(f) + min(i + 3, size(f)));
+			t -= lm + r;
 
-	//		if (i < size(f) - 2)
-	//			mini(f, f[i], i, i + 2);
-	//		if (i > 1)
-	//			mini(f, f[i], i - 2, i);
-	//		if (i < size(f) - 1 && i>0)
-	//			mini(f, f[i], i - 1, i + 1);
+			Printer::print(i, t, lm);
+			Printer::print(l, m, r);
 
-	//		if (f[i] != 0)
-	//			return false;
+			if (t > 0)
+				return false;
+		}
 
-	//		copy(begin(t), end(t), begin(f) + max(0, (int)i - 2));
-	//	}
-
-	//	return true;
-	//}
+		return true;
+	}
 
 	// 165 ms, faster than 50.77% : 69.5 MB, less than 12.99%
 	bool solution2(vector<int> nums) {
@@ -79,12 +80,14 @@ public:
 	vector<int> output = {
 		true,
 		true,
+		false,
 		false
 	};
 	static constexpr int const& inputs = 1;
 	vector<vector<int>> input1 = {
 		{1,2,3,3,4,5},
 		{1,2,3,3,4,4,5,5},
-		{1,2,3,4,4,5}
+		{1,2,3,4,4,5},
+		{3,4,4,5,6,7,8,9,10,11}
 	};
 };
