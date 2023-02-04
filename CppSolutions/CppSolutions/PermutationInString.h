db62d746db0ce8eb9a484ea018e5dcc4573aec92
MKSherbini <mksherbini@gmail.com>
2023-02-04 17:16:53 +0200

    PermutationInString

--- CppSolutions/CppSolutions/PermutationInString.h
+++ CppSolutions/CppSolutions/PermutationInString.h
@@ -7,8 +7,37 @@ using namespace std;
 class PermutationInString
 {
 public:
-	// 4 ms, faster than 97.21% : 7.2 MB, less than 73.61%
+	// 0 ms, faster than 100% : 7.1 MB, less than 98.93%
 	bool solution(string s1, string s2) {
+		vector<int> target(26);
+		for (int i = 0; i < size(s1); i++)
+			target[s1[i] - 'a']++;
+
+		int n = size(s1), chars = n, st = 0;
+		for (int i = 0; i < size(s2); i++) {
+			int charIdx = s2[i] - 'a';
+
+			if (target[charIdx] > 0)
+				--target[charIdx], --chars;
+			else {
+				while (st <= i) {
+					if (s2[st] == s2[i]) {
+						++st;
+						break;
+					}
+					++target[s2[st] - 'a'], ++chars, ++st;
+				}
+			}
+
+			Printer::print(i, chars);
+			if (chars == 0) return true;
+		}
+
+		return false;
+	}
+
+	// 4 ms, faster than 97.21% : 7.2 MB, less than 73.61%
+	bool solution2(string s1, string s2) {
 		for (int i = 0; i < s1.size(); i++)
 			target[s1[i] - 'a']++;
 
@@ -23,7 +52,6 @@ public:
 		}
 		return false;
 	}
-private:
 	int target[26] = { 0 }, found[26] = { 0 };
 	void resetFound() {
 		memset(found, 0, sizeof(int) * 26);
@@ -36,5 +64,19 @@ private:
 			if (found[i]) cout << char(i + 'a') << ": " << found[i] << ", ";
 		cout << endl;
 	}
+
+	vector<int> output = {
+		true,
+		false
+	};
+	static constexpr int const& inputs = 2;
+	vector<string> input1 = {
+		"ab",
+		"ab"
+	};
+	vector<string> input2 = {
+		"aidbaooo",
+		"eidboaoo"
+	};
 };
 
--- CppSolutions/CppSolutions/Runner.cpp
+++ CppSolutions/CppSolutions/Runner.cpp
@@ -1,5 +1,5 @@
 ﻿#include "stdc++.h"
-#include "ZigzagConversion.h"
+#include "PermutationInString.h"
 
 //#include <bits/stdc++.h>
 //using namespace std;
@@ -9,6 +9,6 @@ int main()
 {
 	freopen("output.txt", "w", stdout);
 	freopen("input.txt", "r", stdin);
-	Runner(true).run<ZigzagConversion>();
+	Runner(true).run<PermutationInString>();
 }
 
