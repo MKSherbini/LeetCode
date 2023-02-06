204f2ff7d765d47aca8935979dc7b45489ac6e73
MKSherbini <mksherbini@gmail.com>
2023-02-06 12:07:38 +0200

    ShuffleTheArray

--- CppSolutions/CppSolutions/ShuffleTheArray.h
+++ CppSolutions/CppSolutions/ShuffleTheArray.h
@@ -14,26 +14,24 @@ public:
 			ret[i * 2] = nums[i], ret[i * 2 + 1] = nums[i + n];
 		return ret;
 	}
+
 	vector<vector<int>> output = {
-		{ 2, 3, 5, 4, 1, 7 },
-		{ 1, 4, 2, 3, 3, 2, 4, 1 }
+		{2,3,5,4,1,7},
+		{1,4,2,3,3,2,4,1},
+		{1,2,1,2}
 	};
+	static constexpr int const& inputs = 2;
 	vector<vector<int>> input1 = {
-		{ 2, 5, 1, 3, 4, 7 },
-		{ 1, 2, 3, 4, 4, 3, 2, 1 }
+		{2,5,1,3,4,7},
+		{1,2,3,4,4,3,2,1},
+		{1,1,2,2}
 	};
 	vector<int> input2 = {
 		3,
-		4
+		4,
+		2
 	};
 
 };
 // x => i*2
 // y => (i-n)*2+1 
-
-//run(ShuffleTheArray(),
-//	vector<int> { 2, 3, 5, 4, 1, 7 },
-//	vector<int> { 2, 5, 1, 3, 4, 7 }, 3);
-//run(ShuffleTheArray(),
-//	vector<int> { 1, 4, 2, 3, 3, 2, 4, 1 },
-//	vector<int> { 1, 2, 3, 4, 4, 3, 2, 1 }, 4);
