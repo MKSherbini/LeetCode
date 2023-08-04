b1c6d2c3ecea097f198be6cb70560917693689a5
MKSherbini <mksherbini@gmail.com>
2023-08-04 04:21:08 +0300

    WordBreak

--- CppSolutions/CppSolutions/WordBreak.h
+++ CppSolutions/CppSolutions/WordBreak.h
@@ -7,8 +7,37 @@ using namespace std;
 class WordBreak
 {
 public:
-	// 0 ms, faster than 100% : 7.5 MB, less than 95.42%
+	// 0 ms, faster than 100% : 7.9 MB, less than 81.09%
 	bool solution(string s, vector<string> wordDict) {
+		int n = size(s);
+		set<int> sizes;
+		set<string> words;
+
+		for (auto w : wordDict)
+			sizes.insert(size(w)), words.insert(w);
+
+		vector<int> mem(s.size(), -1);
+		function<bool(int)> f = [&](int i) -> bool {
+			if (i == n) return true;
+			if (mem[i] != -1) return mem[i];
+
+			bool ret = 0;
+
+			for (auto size : sizes) {
+				if (size + i > n) continue;
+				Printer::print(i, size, s.substr(i, size), words.count(s.substr(i, size)));
+				if (!words.count(s.substr(i, size))) continue;
+				ret |= f(i + size);
+			}
+
+			return mem[i] = ret;
+		};
+
+		return f(0);
+	}
+
+	// 0 ms, faster than 100% : 7.5 MB, less than 95.42%
+	bool solution2(string s, vector<string> wordDict) {
 		vector<int> mem(s.size(), -1);
 		auto ret = curse(s, wordDict, mem, 0);
 		Printer::print(mem);
