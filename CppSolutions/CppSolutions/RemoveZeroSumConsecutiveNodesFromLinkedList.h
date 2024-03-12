b23c0efc9086e717a3e0d8333400a03b549517c3
MKSherbini <mksherbini@gmail.com>
2024-03-13 00:55:11 +0200

    RemoveZeroSumConsecutiveNodesFromLinkedList

--- CppSolutions/CppSolutions/RemoveZeroSumConsecutiveNodesFromLinkedList.h
+++ CppSolutions/CppSolutions/RemoveZeroSumConsecutiveNodesFromLinkedList.h
@@ -7,8 +7,63 @@ using namespace std;
 class RemoveZeroSumConsecutiveNodesFromLinkedList
 {
 public:
-	// 3 ms, faster than 97.32% : 13 MB, less than 81.69%
+	// 4 ms, faster than 90.44% : 14.8 MB, less than 49.07%
 	ListNode* solution(ListNode* head) {
+		auto it = new ListNode(0, head);
+		head = it;
+
+		int sum = 0;
+		unordered_map<int, ListNode*> sums;
+
+		while (it != nullptr) {
+			sum += it->val;
+			if (sums.count(sum)) {
+				auto it2 = sums[sum]->next;
+				int sum2 = sum + it2->val;
+				while (sum2 != sum) {
+					sums.erase(sum2);
+					it2 = it2->next;
+					sum2 += it2->val;
+				}
+
+				sums[sum]->next = it->next;
+			}
+			else
+				sums[sum] = it;
+			it = it->next;
+		}
+
+		return head->next;
+	}
+
+	// 3 ms, faster than 97.29% : 14.8 MB, less than 49.07%
+	ListNode* solution3(ListNode* head) {
+		auto it = new ListNode(0, head);
+		head = it;
+
+		int sum = 0;
+		unordered_map<int, ListNode*> sums;
+		sums[0] = it;
+
+		while (it != nullptr) {
+			sum += it->val;
+			sums[sum] = it;
+			it = it->next;
+		}
+
+		sum = 0;
+		it = head;
+		while (it) {
+			sum += it->val;
+			it->next = sums[sum]->next;
+			it = it->next;
+		}
+
+		return head->next;
+	}
+
+	// 3 ms, faster than 97.32% : 13 MB, less than 81.69%
+	ListNode* solution2(ListNode* head) {
 		auto it = new ListNode(0, head);
 		head = it;
 
